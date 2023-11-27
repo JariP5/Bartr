3310f1c0ef4bfa39091834efd9afd96d6762cd00
Jari Polm <jpolm@rollins.edu>
2023-11-27 13:19:58 -0500

    NExt

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,5 @@
 import { Button, Center, Input, InputField, Text, VStack } from '@gluestack-ui/themed';
+import auth from '@react-native-firebase/auth';
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from "react";
@@ -7,13 +8,19 @@ import { StackParamList } from './Navigation/Params';
 
 const Login = () => {
     const navigation = useNavigation<StackNavigationProp<StackParamList>>();
-    const [userName, setUserName] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [validCredentials, setValidCredentials] = useState<Boolean>(true);
 
-    function handleLogin() {
-        setValidCredentials(false);
-    }
+    const handleLogin = async () => {
+        try {
+          const response = await auth().signInWithEmailAndPassword(email, password);
+        //   console.log('User signed in:', response.user);
+        } catch (error) {
+        //   console.error('Sign-in error:', error);
+        //   setValidCredentials(false);
+        }
+    };
 
 
     return (
@@ -22,7 +29,7 @@ const Login = () => {
             <VStack mt={200}>
 
                 <Input w={200}>
-                    <InputField onChangeText={value => setUserName(value)}/>
+                    <InputField onChangeText={value => setEmail(value)}/>
                 </Input>
 
                 <Input w={200}>
