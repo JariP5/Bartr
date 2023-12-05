40ec01b90f5eee21e4bc8f80f4ecd2ce11b5141b
Jari Polm <jpolm@rollins.edu>
2023-12-05 12:29:19 -0500

    Before

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import MyStack from './src/Navigation/MyStack';
 
+
 export default function App() {
   return (
     <GluestackUIProvider config={config}>
